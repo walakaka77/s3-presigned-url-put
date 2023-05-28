3118ca7853f2aaab8a377d4b9f7f2a7e7670c39b
walakaka <ahmadshafik92@gmail.com>
2023-05-28 21:45:00 +0800

    First Commit

---
+++ S3-Presigned-URL-Put/Program.cs
@@ -0,0 +1,107 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+public class UploadUsingPresignedURL
+{
+    public static void Main()
+    {
+        string bucketName = "walakaka-private-bucket";
+        string keyName = "Testfile4.txt";
+        string filePath = $"C:\\Users\\Walakaka\\Downloads\\{keyName}";
+
+        // Specify how long the signed URL will be valid in hours.
+        double timeoutDuration = 12;
+
+        // If the AWS Region defined for your default user is different
+        // from the Region where your Amazon S3 bucket is located,
+        // pass the Region name to the Amazon S3 client object's constructor.
+        // For example: RegionEndpoint.USWest2.
+        IAmazonS3 client = new AmazonS3Client("Access key ID", "Secret access key");
+
+        var url = GeneratePreSignedURL(client, bucketName, keyName, timeoutDuration);
+        var success = UploadObject(filePath, url);
+
+        if (success)
+        {
+            Console.WriteLine("Upload succeeded.");
+        }
+        else
+        {
+            Console.WriteLine("Upload failed.");
+        }
+    }
+
+    /// <summary>
+    /// Uploads an object to an Amazon S3 bucket using the presigned URL passed in
+    /// the url parameter.
+    /// </summary>
+    /// <param name="filePath">The path (including file name) to the local
+    /// file you want to upload.</param>
+    /// <param name="url">The presigned URL that will be used to upload the
+    /// file to the Amazon S3 bucket.</param>
+    /// <returns>A Boolean value indicating the success or failure of the
+    /// operation, based on the HttpWebResponse.</returns>
+    public static bool UploadObject(string filePath, string url)
+    {
+        HttpWebRequest httpRequest = WebRequest.Create(url) as HttpWebRequest;
+        httpRequest.Method = "PUT";
+        using (Stream dataStream = httpRequest.GetRequestStream())
+        {
+            var buffer = new byte[8000];
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                int bytesRead = 0;
+                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    dataStream.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+
+        HttpWebResponse response = httpRequest.GetResponse() as HttpWebResponse;
+        return response.StatusCode == HttpStatusCode.OK;
+    }
+
+    /// <summary>
+    /// Generates a presigned URL which will be used to upload an object to
+    /// an Amazon S3 bucket.
+    /// </summary>
+    /// <param name="client">The initialized Amazon S3 client object used to call
+    /// GetPreSignedURL.</param>
+    /// <param name="bucketName">The name of the Amazon S3 bucket to which the
+    /// presigned URL will point.</param>
+    /// <param name="objectKey">The name of the file that will be uploaded.</param>
+    /// <param name="duration">How long (in hours) the presigned URL will
+    /// be valid.</param>
+    /// <returns>The generated URL.</returns>
+    public static string GeneratePreSignedURL(
+        IAmazonS3 client,
+        string bucketName,
+        string objectKey,
+        double duration)
+    {
+        var request = new GetPreSignedUrlRequest
+        {
+            BucketName = bucketName,
+            Key = objectKey,
+            Verb = HttpVerb.PUT,
+            Expires = DateTime.UtcNow.AddHours(duration),
+        };
+
+        string url = client.GetPreSignedURL(request);
+        return url;
+    }
+}
+
+
+
+
+
+
